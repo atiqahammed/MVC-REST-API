d483f4cdb6f765e524e60577bc1471d1f1a9f2cc
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-08 17:33:34 +0600

    mock repo is ready for command model with dummy data

---
+++ source/Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+	public class MockCommanderRepo : ICommanderRepo
+	{
+		public Command GetCommandById(int id)
+		{
+			return new Command{Id=0, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"};
+		}
+
+		public IEnumerable<Command> GetCommands()
+		{
+			var commads = new List<Command> 
+			{
+				new Command{Id=0, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"},
+				new Command{Id=2, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"},
+				new Command{Id=3, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"},
+				new Command{Id=4, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"},
+				new Command{Id=5, HowTo="Boil ab egg", Line="Boil Water", Platform= "Kettle & pan"},
+			};
+			
+			return commads;
+		}
+	}
+}
