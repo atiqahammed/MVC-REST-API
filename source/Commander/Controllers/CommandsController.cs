c54f3fd6a0c5f2d84e053374ad6b9482da87d829
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-09 13:20:43 +0600

    controller has been added after modifying the thedotnetcore error

---
+++ source/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    // pattern [baseurl]/api/commands
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+    }
+}
