54c252fec9182415a48e26e852182c4329fd59c5
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-06-10 20:06:11 +0600

    data annotation has been added

--- source/Commander/Dtos/CommandCreateDto.cs
+++ source/Commander/Dtos/CommandCreateDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos 
 {
     public class CommandCreateDto
     {
-		public string HowTo { get; set; }
+		[Required]
+        [MaxLength(512)]
+        public string HowTo { get; set; }
+        [Required]
         public string Line { get; set; }
-		public string Platform { get; set; }
+        [Required]
+        public string Platform { get; set; }
     }
 }
